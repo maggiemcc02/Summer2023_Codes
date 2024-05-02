0aa9d212dccedd5e6bac489fff5fbb2e27f9b92a
Maggie McCarthy <108485831+maggiemcc02@users.noreply.github.com>
2024-05-02 14:30:46 -0230

    Adding prints to check if we even perturb the newton step

--- Codes_From_DrMacLachlans_Computer/physical_solver.py
+++ Codes_From_DrMacLachlans_Computer/physical_solver.py
@@ -17,27 +17,12 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
 
-
-
-
 from deflation import *
 from system_and_jacobian import *
 
 
 
-
-
-
-
-
-
-
 # The Line Search
-
-
-
-
-
 
 
 
@@ -229,6 +214,11 @@ def inv_quad_interp_line_search(U_old, delta, a_old, a, System, smile, J_xc, F_x
   #   # print("We have a descent direction!")
   #   # print()
 
+  print()
+  print()
+  print("ARE WE GOING TO PERTURB THE STEP?")
+  print()                            
+
   if check_2 > 0:
 
     print('We dont have a descent direction!')
@@ -307,18 +297,21 @@ def inv_quad_interp_line_search(U_old, delta, a_old, a, System, smile, J_xc, F_x
 
     perturb_step = perturb_system(J_xc, F_xc, smile, System, U_old, mesh, mesh_space,\
                     k, u_0, u_n, E, solutions, sol_mesh, power, alpha)
+
+    print("YES WE WILL PERTURB THE NEWTON STEP!")
+    print()
+    print()
   
 
     # print('Damping Parameter for problem plot is :', a)
     # print()
-
-    print("We are going to Perturb the Newton Step")
-    print()
-    print()
-    
-   
+       
 
   else:
+
+    print("NO WE WILL NOT PERTURB THE NEWTON STEP")
+    print()
+    print()
 
     perturb_step = delta 
 
@@ -339,17 +332,6 @@ def inv_quad_interp_line_search(U_old, delta, a_old, a, System, smile, J_xc, F_x
 
 
 # The Perturbation Codes
-
-
-
-
-
-
-
-
-
-
-
 
 
 
